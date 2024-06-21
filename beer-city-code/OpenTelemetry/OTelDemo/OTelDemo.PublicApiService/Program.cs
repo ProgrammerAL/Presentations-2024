c96fadbd5929238abc53f176b21ca76ac0f67c56
Al Rodriguez <ProgrammerAl@outlook.com>
2024-06-20 22:56:49 -0400

    Some more work on the OTel demo code

--- beer-city-code/OpenTelemetry/OTelDemo/OTelDemo.PublicApiService/Program.cs
+++ beer-city-code/OpenTelemetry/OTelDemo/OTelDemo.PublicApiService/Program.cs
@@ -1,3 +1,5 @@
+using OTelDemo.PublicApiService.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
@@ -9,14 +11,19 @@ builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.CustomSchemaIds(x => x.FullName?.ToString() ?? Guid.NewGuid().ToString());
+    options.CustomSchemaIds(x => x.FullName?.Replace("+", ".").ToString() ?? Guid.NewGuid().ToString());
 });
+
+
+builder.Services.AddHttpClient("internal-api-client",
+    static client => client.BaseAddress = new("https+http://internal-api"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
+GetProductsEndpoint.RegisterApiEndpoint(app);
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
