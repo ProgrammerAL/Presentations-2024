22a3c98e46087369abaa7e4607782bc98b1e0e4f
Al Rodriguez <ProgrammerAl@outlook.com>
2024-10-29 18:02:06 -0400

    Added a script to start everything I need before presenting

---
+++ update-conference-prague-2024/utils/open-presentation-materials.ps1
@@ -0,0 +1,16 @@
+
+$presentationPath = "$PSScriptRoot/../"
+$demoCodeRootPath = "$PSScriptRoot/../demo-code-feedback-system"
+
+$vsExePath = "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/devenv.exe"
+
+& $vsExePath "$demoCodeRootPath/src/FeedbackApp.sln"
+& $vsExePath "$demoCodeRootPath/build/build/Build.sln"
+& $vsExePath "$demoCodeRootPath/infra/PulumiInfra.sln"
+& $vsExePath "$demoCodeRootPath/deploy/deploy/Deploy.sln"
+& $vsExePath "$presentationPath/../"
+
+& "c:/ZoomIt/ZoomIt.exe"
+
+Start-Process -FilePath MSEdge -ArgumentList "$presentationPath/presentation.html", "https://github.com/ProgrammerAL/Presentations-2024/actions", "https://app.pulumi.com/ProgrammerAl/update-conf-2024/dev", "https://portal.azure.com/#browse/resourcegroups"
+
